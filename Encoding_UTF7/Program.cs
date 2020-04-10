503d5bc4513b1947b6ce84eda5288c06a3440116
bbence <borbelybence98@gmail.com>
2020-04-10 15:09:28 +0200

    convert a text to utf7

---
+++ Encoding_UTF7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Encoding_UTF7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName = @"D:\Documents\Codecool\.NET module\SI weeks\Regular Expression\";
+            StreamReader stRead = new StreamReader(FileName + ".gitignore");
+
+            StreamWriter stWrite = new StreamWriter(FileName + ".gitignore_utf7.txt", false, Encoding.UTF7);
+
+            string line;
+            while ((line = stRead.ReadLine()) != null)
+            {
+                stWrite.WriteLine(line);
+            }
+            Console.WriteLine("File had been encoded to: " + FileName + ".gitignore_utf7.txt");
+            stWrite.Close();
+            
+            Console.ReadKey();
+        }
+    }
+}
